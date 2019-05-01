5141c4d992048f77e30e6f380077946ae1ab239e
gregmoncayo <gregorymoncayo25@hotmail.com>
2019-05-01 13:23:34 -0400

    C++ 2

---
+++ Data Structures C++/DoubleLinked.h
@@ -0,0 +1,13 @@
+//
+//  DoubleLinked.h
+//  DoubleLinked
+//
+//  Created by Gregory Moncayo on 3/12/19.
+//  Copyright © 2019 Gregory Moncayo. All rights reserved.
+//
+
+#ifndef DoubleLinked_h
+#define DoubleLinked_h
+
+
+#endif /* DoubleLinked_h */
---
+++ Data Structures C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DoubleLinked
+//
+//  Created by Gregory Moncayo on 3/12/19.
+//  Copyright © 2019 Gregory Moncayo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
